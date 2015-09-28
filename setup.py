96a6994af066838ba0daf840e5100f8bba9748bc
Michael Schwarz <michi.schwarz@gmail.com>
2015-09-28 02:09:52 +0200

    (master)

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(
 	name = 'feuermurmel-events',
 	packages = ['feuermurmel'],
-	install_requires = [''])
+	install_requires = ['https://github.com/Feuermurmel/python-feuermurmel-sched/archive/v0.0.00000001.tar.gz'])
